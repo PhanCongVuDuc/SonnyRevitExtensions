0e9cd1f9bd8ec16710f3be61b56cdf124c8b8707
Phan Cong Vu Duc <phancongvuduc@gmail.com>
2025-11-13 20:33:46 +0700

    move other project

---
+++ Extensions/ToleranceConstants.cs
@@ -0,0 +1,80 @@
+namespace SonnyRevitExtensions.Extensions ;
+
+/// <summary>
+/// Contains tolerance and precision constants used in geometric calculations
+/// </summary>
+public static class ToleranceConstants
+{
+  #region Core Tolerance Constants
+
+  /// <summary>
+  /// High precision for critical calculations (1.0e-09)
+  /// Used for: angle calculations, parameter comparisons, vector operations
+  /// </summary>
+  public const double HighPrecision = 1.0e-09 ;
+
+  /// <summary>
+  /// Standard precision for most geometric operations (0.0001)
+  /// Used for: general floating point comparisons, volume thresholds
+  /// </summary>
+  public const double StandardPrecision = 0.0001 ;
+
+  /// <summary>
+  /// General tolerance for distance and geometric operations (0.001)
+  /// Used for: point distances, vector comparisons, surface areas
+  /// </summary>
+  public const double GeneralTolerance = 0.001 ;
+
+  /// <summary>
+  /// Coarse tolerance for intersection and line operations (0.01)
+  /// Used for: line intersections, face detection
+  /// </summary>
+  public const double CoarseTolerance = 0.01 ;
+
+  /// <summary>
+  /// Very fine tolerance for dot product calculations (0.0000001)
+  /// </summary>
+  public const double DotProductTolerance = 0.0000001 ;
+
+  #endregion
+
+  #region Helper Methods
+
+  /// <summary>
+  /// Checks if two double values are equal within standard precision
+  /// </summary>
+  public static bool AreEqual( double value1,
+    double value2 )
+  {
+    return Math.Abs( value1 - value2 ) < StandardPrecision ;
+  }
+
+  /// <summary>
+  /// Checks if two double values are equal within specified tolerance
+  /// </summary>
+  public static bool AreEqual( double value1,
+    double value2,
+    double tolerance )
+  {
+    return Math.Abs( value1 - value2 ) < tolerance ;
+  }
+
+  /// <summary>
+  /// Checks if a value is zero within standard precision
+  /// </summary>
+  public static bool IsZero( double value )
+  {
+    return Math.Abs( value ) < StandardPrecision ;
+  }
+
+  /// <summary>
+  /// Checks if a value is zero within specified tolerance
+  /// </summary>
+  public static bool IsZero( double value,
+    double tolerance )
+  {
+    return Math.Abs( value ) < tolerance ;
+  }
+
+  #endregion
+}
---
+++ Extensions/XYZs/XYZGeometryExtensions.cs
@@ -0,0 +1,209 @@
+namespace SonnyRevitExtensions.Extensions.XYZs ;
+
+/// <summary>
+/// Extension methods for XYZ geometric calculations
+/// </summary>
+public static partial class XYZGeometryExtensions
+{
+  /// <summary>
+  /// Checks if a point is inside a polygon using ray casting algorithm
+  /// </summary>
+  /// <param name="point">Point to test</param>
+  /// <param name="polygonVertices">Vertices of the polygon</param>
+  /// <returns>True if point is inside polygon</returns>
+  public static bool IsPointInsidePolygon( this XYZ point,
+    List<XYZ> polygonVertices )
+  {
+    var angle = 0.0 ;
+    for ( var i = 0 ; i < polygonVertices.Count ; i++ ) {
+      var j = i == polygonVertices.Count - 1 ? 0 : i + 1 ;
+      var a = new XYZ( polygonVertices[ i ].X,
+        polygonVertices[ i ].Y,
+        0 ) ;
+      var b = new XYZ( point.X,
+        point.Y,
+        0 ) ;
+      var c = new XYZ( polygonVertices[ j ].X,
+        polygonVertices[ j ].Y,
+        0 ) ;
+
+      var ba = Line.CreateBound( b,
+          a )
+        .Direction ;
+      var bc = Line.CreateBound( b,
+          c )
+        .Direction ;
+      angle += ba.AngleTo( bc ) ;
+    }
+
+    return Math.Abs( angle - 2 * Math.PI ) < ToleranceConstants.HighPrecision ;
+  }
+
+  /// <summary>
+  /// Point-in-polygon test with vertex array
+  /// </summary>
+  /// <param name="nvert">Number of vertices</param>
+  /// <param name="vertx">X coordinates of vertices</param>
+  /// <param name="verty">Y coordinates of vertices</param>
+  /// <param name="testx">X coordinate of test point</param>
+  /// <param name="testy">Y coordinate of test point</param>
+  /// <returns>True if point is inside polygon</returns>
+  public static bool PointInPolygon( int nvert,
+    double[] vertx,
+    double[] verty,
+    double testx,
+    double testy )
+  {
+    int i, j ;
+    var c = false ;
+    for ( i = 0, j = nvert - 1 ; i < nvert ; j = i++ ) {
+      if ( verty[ i ] > testy != ( verty[ j ] > testy )
+           && ( testx
+                < ( vertx[ j ] - vertx[ i ] ) * ( testy - verty[ i ] ) / ( verty[ j ] - verty[ i ] ) + vertx[ i ] ) )
+        c = ! c ;
+    }
+
+    return c ;
+  }
+
+  /// <summary>
+  /// Calculates the centroid of a list of points
+  /// </summary>
+  /// <param name="sources">List of points</param>
+  /// <returns>Centroid point</returns>
+  public static XYZ GetCentroid( this List<XYZ> sources )
+  {
+    var centroid = sources[ 0 ] ;
+    for ( var i = 1 ; i < sources.Count ; i++ ) {
+      centroid = centroid.Add( sources[ i ] ) ;
+    }
+
+    return centroid.Divide( sources.Count ) ;
+  }
+
+  /// <summary>
+  /// Checks if three points form a clockwise orientation
+  /// </summary>
+  /// <param name="firstPoint">First point</param>
+  /// <param name="secondPoint">Second point</param>
+  /// <param name="thirdPoint">Third point</param>
+  /// <returns>True if clockwise</returns>
+  public static bool IsClockwise( XYZ firstPoint,
+    XYZ secondPoint,
+    XYZ thirdPoint )
+  {
+    var v12 = secondPoint - firstPoint ;
+    var v13 = thirdPoint - firstPoint ;
+    return v12.CrossProduct( v13 )
+             .Z
+           < 0 ;
+  }
+
+  /// <summary>
+  /// Calculates intersection point of two lines defined by points and directions
+  /// </summary>
+  /// <param name="firstLinePoint">Point on first line</param>
+  /// <param name="firstLineDirection">Direction of first line</param>
+  /// <param name="secondLinePoint">Point on second line</param>
+  /// <param name="secondLineDirection">Direction of second line</param>
+  /// <returns>Intersection point or null if lines are parallel</returns>
+  public static XYZ? CalculateIntersection( XYZ firstLinePoint,
+    XYZ firstLineDirection,
+    XYZ secondLinePoint,
+    XYZ secondLineDirection )
+  {
+    // Ensure we're working with 2D: zero out the Z component
+    firstLinePoint = new XYZ( firstLinePoint.X,
+      firstLinePoint.Y,
+      0 ) ;
+    secondLinePoint = new XYZ( secondLinePoint.X,
+      secondLinePoint.Y,
+      0 ) ;
+    firstLineDirection = new XYZ( firstLineDirection.X,
+      firstLineDirection.Y,
+      0 ).Normalize() ;
+    secondLineDirection = new XYZ( secondLineDirection.X,
+      secondLineDirection.Y,
+      0 ).Normalize() ;
+
+    // Calculate determinants
+    var denominator = firstLineDirection.X * secondLineDirection.Y - firstLineDirection.Y * secondLineDirection.X ;
+
+    if ( Math.Abs( denominator ) < ToleranceConstants.HighPrecision ) {
+      return null ;
+    }
+
+    var t = ( ( secondLinePoint.X - firstLinePoint.X ) * secondLineDirection.Y
+              - ( secondLinePoint.Y - firstLinePoint.Y ) * secondLineDirection.X )
+            / denominator ;
+    var intersectionPoint = new XYZ( firstLinePoint.X + t * firstLineDirection.X,
+      firstLinePoint.Y + t * firstLineDirection.Y,
+      0 ) ;
+
+    return intersectionPoint ;
+  }
+
+  /// <summary>
+  /// Checks if angle between two vectors is acute (less than 90 degrees) in 2D
+  /// </summary>
+  /// <param name="first">First vector</param>
+  /// <param name="second">Second vector</param>
+  /// <param name="third">Third vector</param>
+  /// <returns>True if angle is acute</returns>
+  public static bool IsAcuteAngle2D( XYZ first,
+    XYZ second,
+    XYZ third )
+  {
+    var vector1 = first - second ;
+    var vector2 = third - second ;
+
+    // Convert to 2D
+    vector1 = new XYZ( vector1.X,
+      vector1.Y,
+      0 ) ;
+    vector2 = new XYZ( vector2.X,
+      vector2.Y,
+      0 ) ;
+
+    var dotProduct = vector1.DotProduct( vector2 ) ;
+    return dotProduct > 0 ;
+  }
+
+  /// <summary>
+  /// Checks if three points are collinear in 2D
+  /// </summary>
+  /// <param name="lineStartPoint">First point</param>
+  /// <param name="lineEndPoint">Second point</param>
+  /// <param name="testPoint">Third point</param>
+  /// <param name="tolerance">Tolerance for comparison</param>
+  /// <returns>True if points are collinear</returns>
+  public static bool IsPointOnLineSegment2D( XYZ lineStartPoint,
+    XYZ lineEndPoint,
+    XYZ testPoint,
+    double tolerance = ToleranceConstants.GeneralTolerance )
+  {
+    var distanceLine = lineStartPoint.DistanceTo2D( lineEndPoint ) ;
+    var distanceToFrom = lineStartPoint.DistanceTo2D( testPoint ) ;
+    var distanceToEnd = lineEndPoint.DistanceTo2D( testPoint ) ;
+    return Math.Abs( distanceLine - distanceToFrom - distanceToEnd ) < tolerance ;
+  }
+
+  /// <summary>
+  /// Checks if three points are collinear in 3D
+  /// </summary>
+  /// <param name="lineStartPoint">First point</param>
+  /// <param name="lineEndPoint">Second point</param>
+  /// <param name="testPoint">Third point</param>
+  /// <param name="tolerance">Tolerance for comparison</param>
+  /// <returns>True if points are collinear</returns>
+  public static bool IsPointOnLineSegment3D( XYZ lineStartPoint,
+    XYZ lineEndPoint,
+    XYZ testPoint,
+    double tolerance = ToleranceConstants.GeneralTolerance )
+  {
+    var distanceLine = lineStartPoint.DistanceTo( lineEndPoint ) ;
+    var distanceToFrom = lineStartPoint.DistanceTo( testPoint ) ;
+    var distanceToEnd = lineEndPoint.DistanceTo( testPoint ) ;
+    return Math.Abs( distanceLine - distanceToFrom - distanceToEnd ) < tolerance ;
+  }
+}
