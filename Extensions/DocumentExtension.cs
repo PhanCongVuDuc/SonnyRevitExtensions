2de6cb405b7ed136a7bdec3c3d383003b05e48da
Phan Cong Vu Duc <phancongvuduc@gmail.com>
2025-11-18 19:06:08 +0700

    add DocumentExtension

---
+++ Extensions/DocumentExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace PentaOceanApp.RevitExtensions ;
+
+public static class DocumentExtension
+{
+    public static TElement? GetElementById<TElement>(this Document document,
+        string uniqueId) where TElement : Element
+    {
+        if (string.IsNullOrEmpty(uniqueId))
+        {
+            return null ;
+        }
+
+        try
+        {
+            return document.GetElement(uniqueId) as TElement ;
+        }
+        catch
+        {
+            return null ;
+        }
+    }
+
+    public static TElement? GetElementById<TElement>(this Document document,
+        ElementId elementId) where TElement : Element =>
+        ElementId.InvalidElementId == elementId ? null : document.GetElement(elementId) as TElement ;
+}
