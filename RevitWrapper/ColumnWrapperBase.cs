fe6d78422ecf099f44ff999221d3a8923dcb7396
Phan Cong Vu Duc <phancongvuduc@gmail.com>
2025-11-22 14:52:05 +0700

    add ColumnWrapperBase class

---
+++ RevitWrapper/ColumnWrapperBase.cs
@@ -0,0 +1,12 @@
+// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+namespace SonnyRevitExtensions.RevitWrapper ;
+
+/// <summary>
+/// Base wrapper class for Revit Column (FamilyInstance)
+/// </summary>
+public class ColumnWrapperBase(FamilyInstance familyInstance) : FamilyInstanceWrapperBase(familyInstance)
+{
+}
+
